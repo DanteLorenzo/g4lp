54f931c05a0c977e56ad0da6dddaad285efa92b9
Dante Lorenzo <n1emand4me@gmail.com>
2022-02-19 21:41:59 +0300

    Plumber, notify, file-include, size, if, htmlmin and webp-html-nosvg were added.

--- gulp/task/html.js
+++ gulp/task/html.js
@@ -1,6 +1,18 @@
 //HTML
 const html = () => {
     return $.gulp.src($.path.html.src)
+        .pipe($.gp.plumber({
+            errorHandler: $.gp.notify.onError(error => ({
+                title: 'HTML',
+                message: error.message
+            }))
+        }))
+        .pipe($.gp.fileInclude())
+        .pipe($.gp.webpHtmlNosvg())
+        .pipe($.gp.size($.app.size))
+        .pipe($.gp.if($.app.isDev, $.gp.htmlBeautify()))
+        .pipe($.gp.htmlmin($.app.htmlmin))
+        .pipe($.gp.size($.app.size))
         .pipe($.gulp.dest($.path.html.dest))
         .pipe($.browserSync.stream())
 }
